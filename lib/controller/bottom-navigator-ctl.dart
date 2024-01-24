bd3a2769dd41d5852f382c005b2537f8ace42b05
inhohwangg <sk1440sk@naver.com>
2024-01-24 23:48:01 +0900

    bottomNavigator 추가

--- lib/controller/bottom-navigator-ctl.dart
+++ lib/controller/bottom-navigator-ctl.dart
@@ -1,11 +1,23 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:personal_blog/pages/home-page.dart';
 import 'package:personal_blog/pages/word-list.dart';
+import 'package:personal_blog/pages/word-write-page.dart';
+
+import '../global/g_print.dart';
 
 class BottomNavigatorController extends GetxController {
   RxInt selectedIndex = 0.obs;
   RxList pageList = [
     HomePage(),
+    WordWritePage(),
     WordListPage(),
   ].obs;
+
+  onItemTapped(index) {
+    printYellow(index);
+    selectedIndex.value = index;
+  }
+
+  Widget get currentPage => pageList[selectedIndex.value];
 }
