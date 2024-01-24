ca507679d301911c8ee34be5f1df70726d3c29e1
inhohwangg <sk1440sk@naver.com>
2024-01-25 00:31:33 +0900

    게시글 작성후 페이지 이동 , 게시글 호출

--- lib/controller/word-write-page-controller.dart
+++ lib/controller/word-write-page-controller.dart
@@ -3,12 +3,21 @@ import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:personal_blog/controller/bottom-navigator-ctl.dart';
 import 'package:personal_blog/global/g_dio.dart';
 import 'package:personal_blog/global/g_value.dart';
+
+import 'word-list-page-controller.dart';
 
 class WordWritePageController extends GetxController {
   late TextEditingController title = TextEditingController();
   late TextEditingController content = TextEditingController();
+  BottomNavigatorController bottomNavigatorController =
+      Get.put(BottomNavigatorController());
+  WordListPageController wordListPageController =
+      Get.put(WordListPageController());
+  DateTime formattedDate =
+      DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
 
   RxBool writeDone = false.obs;
 
@@ -21,13 +30,17 @@ class WordWritePageController extends GetxController {
         data: {
           'title': title.text,
           'content': content.text,
-          'selectDate': "2024-01-20 10:00:00.123Z"
+          'selectDate': formattedDate.toString(),
         },
       );
       if (res.statusCode == 400) {
         throw Error();
+      } else {
+        title.text = '';
+        content.text = '';
+        bottomNavigatorController.selectedIndex.value = 0;
+        wordListPageController.getData();
       }
-      Get.back();
     } catch (e, stackTrace) {
       print(e);
       print(stackTrace);
