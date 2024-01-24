47175b52ee5f7376e3ecc8b8393072f6792b8cad
inhohwangg <sk1440sk@naver.com>
2024-01-25 00:32:01 +0900

    게시글 목록 homepage 에 추가

--- lib/pages/home-page.dart
+++ lib/pages/home-page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:personal_blog/controller/home-page-controller.dart';
+import 'package:personal_blog/controller/word-list-page-controller.dart';
 import 'package:personal_blog/pages/word-list.dart';
 import 'package:personal_blog/pages/word-write-page.dart';
 
@@ -9,6 +10,8 @@ class HomePage extends GetView<HomePageController> {
 
   @override
   Widget build(BuildContext context) {
+    WordListPageController wordListPageController =
+        Get.put(WordListPageController());
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.blue[100]!.withOpacity(0.25),
@@ -16,37 +19,39 @@ class HomePage extends GetView<HomePageController> {
         automaticallyImplyLeading: false,
         title: Text('HOME'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Get.to(WordWritePage());
-              },
-              child: Text(
-                '글 작성',
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Obx(
+                () => ListView.builder(
+                  itemCount: wordListPageController.postList.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: EdgeInsets.only(top: 15),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(7.5),
+                        color: Colors.amber[400]!.withOpacity(
+                          0.25,
+                        ),
+                      ),
+                      child: Center(
+                        child: Text(
+                          wordListPageController.postList[index]['title'],
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                Get.to(WordListPage());
-              },
-              child: Text(
-                '작성 목록',
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
