65cca095e58f173c1377d6395f9f5f54402cc155
inhohwangg <sk1440sk@naver.com>
2024-04-17 00:43:35 +0900

    이력페이지 UI 수정

--- lib/bin/pages/todo-history-pg.dart
+++ lib/bin/pages/todo-history-pg.dart
@@ -1,15 +1,74 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:personal_blog/bin/controllers/todo-history-pg-ctl.dart';
+import 'package:personal_blog/bin/controllers/todo-pg-ctl.dart';
 
-class TodoHistoryPage extends GetView<TodoHistoryPageController> {
+class TodoHistoryPage extends GetView {
   TodoHistoryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TodoPageController controller = Get.put(TodoPageController());
     return Scaffold(
       appBar: AppBar(
-        title: Text('Todo History Page'),
+        title: Text(
+          'Todo 이력',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            fontStyle: FontStyle.normal,
+          ),
+        ),
+      ),
+      body: Obx(
+        () => controller.isLoading.value
+            ? Expanded(
+                child: RefreshIndicator(
+                  onRefresh: () {
+                    return controller.todoDataGet();
+                  },
+                  child: ListView.builder(
+                    physics: AlwaysScrollableScrollPhysics(
+                        parent: BouncingScrollPhysics()),
+                    itemCount: controller.todoList.length,
+                    itemBuilder: (context, index) {
+                      Map item = controller.todoList[index];
+                      return Container(
+                        decoration: BoxDecoration(
+                            color: Colors.blue[600]!.withOpacity(0.5),
+                            borderRadius: BorderRadius.circular(10)),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 10,
+                        ),
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              item['title'],
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Text(
+                              item['description'],
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            Text(
+                              item['created_at'].split('T')[0].toString(),
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              )
+            : Container(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
       ),
     );
   }
