2971fc6c49abf276a203038e28a41c8efd779943
inhohwangg <sk1440sk@naver.com>
2024-04-17 00:52:07 +0900

    현황 페이지 처럼 적용

--- lib/bin/pages/todo-history-pg.dart
+++ lib/bin/pages/todo-history-pg.dart
@@ -21,47 +21,87 @@ class TodoHistoryPage extends GetView {
       ),
       body: Obx(
         () => controller.isLoading.value
-            ? Expanded(
-                child: RefreshIndicator(
-                  onRefresh: () {
-                    return controller.todoDataGet();
-                  },
-                  child: ListView.builder(
-                    physics: AlwaysScrollableScrollPhysics(
-                        parent: BouncingScrollPhysics()),
-                    itemCount: controller.todoList.length,
-                    itemBuilder: (context, index) {
-                      Map item = controller.todoList[index];
-                      return Container(
-                        decoration: BoxDecoration(
-                            color: Colors.blue[600]!.withOpacity(0.5),
-                            borderRadius: BorderRadius.circular(10)),
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 10,
-                          vertical: 10,
+            ? Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                            onPressed: () {
+                              controller.dayMinus();
+                            },
+                            icon: Icon(Icons.arrow_back_ios_new)),
+                        SizedBox(
+                          width: 10,
                         ),
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              item['title'],
-                              style: TextStyle(color: Colors.white),
+                        Obx(
+                          () => Container(
+                            padding: EdgeInsets.symmetric(vertical: 10),
+                            child: Text(
+                              controller.now.toString().split(' ')[0],
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  fontStyle: FontStyle.normal),
                             ),
-                            Text(
-                              item['description'],
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            Text(
-                              item['created_at'].split('T')[0].toString(),
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
+                          ),
                         ),
-                      );
-                    },
-                  ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        IconButton(
+                            onPressed: () {
+                              controller.dayPlus();
+                            },
+                            icon: Icon(Icons.arrow_forward_ios)),
+                      ],
+                    ),
+                    Expanded(
+                      child: RefreshIndicator(
+                        onRefresh: () {
+                          return controller.todoDataGet();
+                        },
+                        child: ListView.builder(
+                          physics: AlwaysScrollableScrollPhysics(
+                              parent: BouncingScrollPhysics()),
+                          itemCount: controller.todayDataList.length,
+                          itemBuilder: (context, index) {
+                            Map item = controller.todayDataList[index];
+                            return Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.blue[600]!.withOpacity(0.5),
+                                  borderRadius: BorderRadius.circular(10)),
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 10,
+                              ),
+                              margin: EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 5),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    item['title'],
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  Text(
+                                    item['description'],
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  Text(
+                                    item['created_at'].split('T')[0].toString(),
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               )
             : Container(
